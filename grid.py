6ae2837561e34e6b2deed425c7f0c09fe5cac1a6
sdfgh5343 <40606216+sdfgh5343@users.noreply.github.com>
2021-05-21 11:15:33 +0800

    Create grid.py

---
+++ grid.py
@@ -0,0 +1,28 @@
+import re
+import numpy as np
+import pandas as pd
+import matplotlib.tri as mtri
+
+class grid():
+    """Loading hgrid.ll file information"""
+    def __init__(self,filename):
+        """
+        Parameters
+        ----------
+        filename : Filename including path.
+        Returns lon, lat, dep, elem storing into class
+        -------
+        """
+        self.path = filename
+        with open(self.path,'r') as rf:
+            rf.readline()
+            elem,node=re.findall('(\d+)',rf.readline())
+            elem=int(elem);node=int(node)
+        nodeAll=pd.read_csv(self.path,sep='\s+',header=None,skiprows=2,nrows=node)
+        self.lon = np.float64(nodeAll[1])
+        self.lat = np.float64(nodeAll[2])
+        self.depth   = np.float64(nodeAll[3])
+        elemAll = pd.read_csv(self.path,sep='\s+',header=None,skiprows=2+node,nrows=elem)
+        self.element = np.int64([elemAll[2],elemAll[3],elemAll[4]]).transpose()-1
+    def triang(self):
+        return mtri.Triangulation(self.lon,self.lat,self.element)
